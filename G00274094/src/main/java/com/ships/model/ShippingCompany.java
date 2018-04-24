07f809debfdd4cf5378cf4f9aad1358794ff8355
Conor Raftery <c-raftery@hotmail.com>
2018-04-24 16:23:04 +0100

    Show Ships, Shipping Companies working, working on Add Ships

--- G00274094/src/main/java/com/ships/model/ShippingCompany.java
+++ G00274094/src/main/java/com/ships/model/ShippingCompany.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.criteria.Order;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -20,15 +20,20 @@ public class ShippingCompany {
 	
 	@Id
 	@GeneratedValue
-	private int scid;
+	private Long scid;
+	
 	@NotNull
     @Size(min=1,max=250)
 	private String name;
+	
 	@NotNull
     @Size(min=1,max=250)
 	private String homePort;
+	
 	@NotNull
+	@Min(value = 1)
 	private BigDecimal balance;
+	
 	@OneToMany(mappedBy="shippingCompany")
 	private List<Ship>  
 	ships = new ArrayList<Ship>();
@@ -38,10 +43,10 @@ public class ShippingCompany {
 	orders = new ArrayList<OrderInfo>();
 
 	
-	public int getScid() {
+	public Long getScid() {
 		return scid;
 	}
-	public void setScid(int scid) {
+	public void setScid(Long scid) {
 		this.scid = scid;
 	}
 	public String getName() {
